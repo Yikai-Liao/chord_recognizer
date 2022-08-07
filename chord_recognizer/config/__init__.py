3f21c779165b80e9d4528bb743f48e820836d6a4
lyk <m15805383399@163.com>
2022-08-07 21:01:58 +0800

    限制读取文件时使用UTF-8

--- chord_recognizer/config/__init__.py
+++ chord_recognizer/config/__init__.py
@@ -5,7 +5,7 @@ import os
 _CUR_DIR = os.path.split(__file__)[0]
 _CHORD_CONFIG_PATH = os.path.join(_CUR_DIR, "chord_config.npz")
 _GEN_CONFIG_CODE_PATH = os.path.join(_CUR_DIR, "gen_config.py")
-with open(_GEN_CONFIG_CODE_PATH) as f:
+with open(_GEN_CONFIG_CODE_PATH, encoding="UTF-8") as f:
     _CODE_HASH = hash(f.read())
 
 if not (os.path.isfile(_CHORD_CONFIG_PATH) and np.load(_CHORD_CONFIG_PATH)['hash'] == _CODE_HASH):
